e3fded5a688af67002d4f54f1efba64fd8ffd8ec
Kostiantyn Ivashchenko <civaschenko@yahoo.com>
2022-12-21 19:54:25 +0200

    added main views drafts

--- places/urls.py
+++ places/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import MenuList
+from .views import MenuList, MenuDetail, RestaurantList, RestaurantDetail
 
 
 urlpatterns = [
-    path('menus/', MenuList.as_view()),
+    path("menus/", MenuList.as_view()),
+    path("menus/<int:pk>/", MenuDetail.as_view()),
+    path("restaurants/", RestaurantList.as_view()),
+    path("restaurants/<int:pk>/", RestaurantDetail.as_view()),
 ]
--- places/views.py
+++ places/views.py
@@ -4,8 +4,29 @@ from rest_framework import generics
 from .models import Restaurant, Menu
 from .serializers import RestaurantSerializer, MenuSerializer
 
-
+# ----
+# Menu
+# ----
 
 class MenuList(generics.ListAPIView):
     queryset = Menu.objects.filter(actual_date=date.today())
     serializer_class = MenuSerializer
+
+    
+class MenuDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+# ----------    
+# Restaurant
+# ----------
+
+class RestaurantList(generics.ListAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    
+
+class RestaurantDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
